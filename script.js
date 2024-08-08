d4e9559c9621d6b50837095450ba3b57e429db8c
darkskullz9 <djskullz971@gmail.com>
2024-08-08 15:09:02 +0200

    modified:   index.html modified:   script.js modified:   styles/style.css

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const section = document.querySelector('.bg-image');
+const headerHeight = document.querySelector('.header').offsetHeight;
+
+function setSectionHeight() {
+    section.style.height = `calc(100vh - ${headerHeight}px)`;
+}
+
+window.addEventListener('resize', setSectionHeight);
+
+setSectionHeight();
+
 // window.addEventListener("scroll", function() {
 //     const header = document.querySelector('header');
 //     const scrollTop = window.pageXOffset || document.documentElement.scrollTop;
