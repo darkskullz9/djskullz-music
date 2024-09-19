f7000f6de635012b308bf99a36ed0d541dad6ea5
Darkskullz9 <praucasteven@gmail.com>
2024-09-19 13:23:39 +0200

    "Refactored HTML and CSS files, removed style.css, updated index.html and releases.html, modified scripts/releasesModal.js, and added new styles to theme.css and index.css."

--- scripts/releasesModal.js
+++ scripts/releasesModal.js
@@ -1,7 +1,7 @@
 const modal = document.getElementById("releaseModal");
-const modalContent = modal.querySelector('.modal-content')
+const modalContent = modal.querySelector('.modal-content');
 
-function openModal(card) {
+function createModalContent(card) {
 	const cover = card.querySelector('.cover').src;
 	const title = card.querySelector('h2').textContent;
 	const date = card.querySelector('p').textContent;
@@ -12,7 +12,7 @@ function openModal(card) {
 		soundcloud: card.getAttribute('data-soundcloud')
 	};
 
-	const linksHTML = Object.entries(links) .map(([key, url]) => {
+	const linksHTML = Object.entries(links).map(([key, url]) => {
 		if(url) {
 			return `<a href="${url}" target="_blank" rel="noopener">
 						<i class="fa-brands fa-${key} icons"></i>
@@ -21,7 +21,7 @@ function openModal(card) {
 		return '';
 	}).join('');
 
-	modalContent.innerHTML = `
+	return `
 		<span id="closeModal" class="close">Back</span>
 		<img src="${cover}" alt="${title} cover" class="modal-cover">	
 		<div class="modal-info">
@@ -33,23 +33,32 @@ function openModal(card) {
 			</div>
 		</div>
 	`;
+};
 
+function openModal(card) {
+	modalContent.innerHTML = createModalContent(card);
 	modal.style.display = "flex";
 	modal.style.alignItems = "center";
-
-	document.getElementById("closeModal").addEventListener("click", closeModal);
-}
+};
 
 function closeModal() {
 	modal.style.display = "none";
-}
+};
+
+modalContent.addEventListener("click", (event) => {
+	if(event.target.id === "closeModal") {
+		closeModal();
+	}
+});
+
+document.addEventListener("click", (event) => {
+	if(event.target.id === "releaseModal") {
+		closeModal();
+	}
+});
 
 document.querySelectorAll('.card').forEach(card => {
 	card.addEventListener("click", () => openModal(card));
 });
 
-window.addEventListener("click", (event) => {
-	if(event.target === modal) {
-		closeModal ();
-	}
-});
+console.log(closeModal);
